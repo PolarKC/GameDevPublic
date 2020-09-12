f92010afa2f1f3340efef18cff9898cd4e7dfd34
PolarKC <polarongm@gmail.com>
2020-09-11 21:47:27 -0500

    Add ragdoll write up to IK_Animation_Saga and re-organize

---
+++ Notes/IK_Animation_Saga/Code/RagdollController.cs
@@ -0,0 +1,75 @@
+// BROKEN RagdollController - Needs to set rigidbodies to isKinematic
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class RagdollController : NetworkBehaviour
+{
+    public Animator animator;
+    public List<Collider> RagdollColliders = new List<Collider>();
+
+    private Rigidbody rigid;
+    public Rigidbody RIGID_BODY
+    {
+        get
+        {
+            if (rigid == null)
+            {
+                rigid = GetComponentInParent<Rigidbody>();
+            }
+            return rigid;
+        }
+    }
+
+    private void Awake()
+    {
+        SetRagdollParts();
+    }
+
+    private void SetRagdollParts()
+    {
+        Collider[] colliders = this.gameObject.GetComponentsInChildren<Collider>();
+
+        foreach (Collider c in colliders)
+        {
+            if (c.gameObject != this.gameObject)
+            {
+                c.isTrigger = true;
+                RagdollColliders.Add(c);
+            }
+        }
+    }
+
+    public void TurnOnRagdoll()
+    {
+        // Modify components on the root character gameobject
+        RIGID_BODY.useGravity = false;
+        RIGID_BODY.velocity = Vector3.zero;
+        this.gameObject.GetComponentInParent<CapsuleCollider>().enabled = false;
+        animator.enabled = false;
+
+        // Modify body parts
+        foreach (Collider c in RagdollColliders)
+        {
+            c.isTrigger = false;
+            if (c.attachedRigidbody)
+                c.attachedRigidbody.velocity = Vector3.zero;
+        }
+    }
+
+    public void TurnOffRagdoll()
+    {
+        // Modify components on the root character gameobject
+        RIGID_BODY.useGravity = true;
+        RIGID_BODY.velocity = Vector3.zero;
+        this.gameObject.GetComponentInParent<CapsuleCollider>().enabled = true;
+        animator.enabled = true;
+
+        // Modify body parts
+        foreach (Collider c in RagdollColliders)
+        {
+            c.isTrigger = true;
+        }
+    }
+}
--- Notes/IK_Animation_Saga/Ik_Animation_Saga.md
+++ Notes/IK_Animation_Saga/Ik_Animation_Saga.md
@@ -1,7 +1,21 @@
-# Animation + IK + Mirror = Learning Opportunity
+# Animation + IK + Mirror + Ragdolls = A lesson in GameDev debugging, research, and persistence
 
-UPDATE: I found a solution, [see here](#Solution-Found)
+UPDATE: I found a solution to the IK timing problem, [see here](#Solution-Found)
 
+## Ragdolls
+This all started when I couldn't figure out why my character seemed to shoot in a direction that it wasn't facing visually. Imagine pointing your gun at a tree in front of you, pulling the trigger, and seeing the bullet impact into the ground 10 feet to the left.... oh and the muzzle flash seemed to appear from thin air to your left instead of from your weapon.
+
+I slowly teased out every script and component until my player character was just the absolute bare minumum and it STILL shot in the wrong direction.
+
+So, I pulled in a fresh player model and slowly added scripts and components making sure to test each time. It worked just fine... this was maddening. The difference? All of the rigidbodies, colliders, and joints on the original character from running the Unity Ragdoll Wizard.
+
+I wrote a ragdoll controller to enable and disable these elements but I missed one critical step :)
+Take a look at the diff here between the working RagdollerController and the broken one.
+
+
+You have to set all of the colliders to isTrigger -AND- set all rigidbodies to isKinematic. Forgetting one or the other could lead to a ton of headache and wasted time.
+
+## Unity Execution Order O_o
 In this screenshot there is a single player who is the Host of the game.
 
 ![pic](Images/Host_With_IK.png)
